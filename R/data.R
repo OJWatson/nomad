45f865c2394d07949efb639832aba5a80a843f50
OJWatson <oj.watson@hotmail.co.uk>
2023-07-20 21:59:20 +0100

    Very basic model class and more vignettes for structure and data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Zambia 2020 Call Data Records Exponential Departure-Diffusion Model
+#'
+#' Mobility model generated using [mobility::mobility()] based on call data
+#' records collected in Zambia in 2020 The mobility model is an exponential
+#' Departure-Diffusion model.
+#'
+#' For full metadata of the CDR data, please see `?nomad::mobility_database`
+#'
+#' @format A `nomad::model` class object
+#' @source \url{https://elifesciences.org/articles/68441}
+"zambia_cdr_2020_mod_dd_exp"
---
+++ R/model.R
@@ -0,0 +1,41 @@
+#' @title R6 Class for mobility models
+#'
+#' @description A nomad mobility model
+#'
+#' @importFrom R6 R6Class
+R6_nomad_model <- R6::R6Class(
+  classname = "nomad.model",
+  cloneable = FALSE,
+
+  # PUBLIC METHODS
+  public = list(
+
+    # INITIALISATION
+    #' @description
+    #' Create a new `nomad` mobility model
+    #' @param model [mobility::mobility()] model
+    #' @param model_desc Short description of model type and underlying data
+    #' @param data_desc Short description of data used to fit `model`
+    #' @param model_name The name of the model for linking with `nomad::model_data`
+    #' @return A new `nomad.model` object.
+    initialize = function(model,
+                          model_desc,
+                          data_desc,
+                          model_name) {
+
+      private$model <- model
+      private$model_desc <- model_desc
+      private$data_desc <- data_desc
+      private$model_name <- model_name
+
+    }
+  ),
+
+  private = list(
+    model = NULL,
+    model_desc = NULL,
+    data_desc = NULL,
+    model_name = NULL
+  )
+)
+
