be8353abb8a9c8f819a38c6bcd500736f82d7310
OJWatson <oj.watson@hotmail.co.uk>
2023-11-10 12:33:57 +0000

    Add map visualisation of data

--- R/helpers.R
+++ R/helpers.R
@@ -3,11 +3,30 @@
 print_mobility_data <- function(data_name) {
 
   m <- which(nomad::mobility_db$name == data_name)
-  message(paste(
-    names(nomad::mobility_db[m, ]),
-    nomad::mobility_db[m, ],
+
+  cat(paste(
+    crayon::yellow$bold(
+      names(rename_mobility_db()[m, ])
+    ),
+    vapply(nomad::mobility_db[m, ], as.character, character(1)),
     sep = ": ",
     collapse = "\n"
   ))
 
+}
+
+#' Rename mobility db with nice names
+#' @noRd
+rename_mobility_db <- function() {
+  mobility_db %>%
+    rename(Name = name) %>%
+    rename(Data = type) %>%
+    rename(ISO3C = country) %>%
+    rename(N = n) %>%
+    rename(Scheme = sampling_scheme) %>%
+    rename(Censoring = censoring) %>%
+    rename(Space = aggregation) %>%
+    rename(URL = publication) %>%
+    rename(Start = date_start) %>%
+    rename(End = date_end)
 }
